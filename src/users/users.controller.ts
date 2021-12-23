a2fb5449f0db41a4d59ed48f3108ff58718ad5d0
Cho Won Jae <yoteamo7@naver.com>
2021-12-23 20:47:53 +0900

    API 설계하기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorator/user.decorator';
 import { userDto } from 'src/common/dto/user.dto';
+import { UndefinedToNullInterceptor } from 'src/common/interceptors/undefinedToNull.interceptor';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
 
+@UseInterceptors(UndefinedToNullInterceptor)
 @ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
@@ -12,8 +23,8 @@ export class UsersController {
   @ApiResponse({ status: 200, description: '성공', type: userDto })
   @ApiOperation({ summary: '내정보 조회' })
   @Get()
-  getUsers(@Req() req) {
-    return req.user;
+  getUsers(@User() user) {
+    return user;
   }
 
   @ApiOperation({ summary: '회원가입' })
@@ -25,8 +36,8 @@ export class UsersController {
   @ApiResponse({ status: 200, description: '성공', type: userDto })
   @ApiOperation({ summary: '로그인' })
   @Post('login')
-  login(@Req() req) {
-    return req.user;
+  login(@User() user) {
+    return user;
   }
 
   @ApiOperation({ summary: '로그아웃' })
